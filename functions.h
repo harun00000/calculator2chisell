c8edc066812cd00525471bcde61f08328fdf6b41
harun00000 <ut14kutsko41@gmail.com>
2025-09-18 20:06:59 +0300

    код готов

--- functions.h
+++ functions.h
@@ -1,13 +1,13 @@
 #include <stdio.h>
-double sum(double num1, double num2){
+double sum(double num1, double num2){    // суммирование (summ)
     return num1+num2;
-}
-double diff(double num1, double num2){
+} 
+double diff(double num1, double num2){   // вычитание (difference)
     return num1-num2;
 }
-double mult(double num1, double num2){
+double mult(double num1, double num2){   // умножение (multiplication)
     return num1*num2;
 }
-double div(double num1, double num2){
+double div(double num1, double num2){    // деление (division)
     return num1/num2;
 }
--- main.c
+++ main.c
@@ -1,12 +1,12 @@
-#include <functions.h>
-double num1=0.0, num2=0.0;
+#include "functions.h"
+double num1=0.0, num2=0.0;   //объявление переменных
 char operator=0; 
 
 int main(){
 printf("Введите выражение\n");
-scanf("%lf %c %lf", &num1, &operator, &num2); //пользователь вводит числа
+scanf("%lf %c %lf", &num1, &operator, &num2);    //пользователь вводит числа
 
-if (operator == '+'){
+if (operator == '+'){                            // проверка операторов
     printf("%lf\n", sum(num1, num2));
 } else if (operator == '-'){
     printf("%lf\n", diff(num1, num2));
@@ -14,11 +14,12 @@ if (operator == '+'){
     printf("%lf\n", mult(num1, num2));
 } else if (operator=='/'){
     if (num2==0){
-        printf("деление на 0");
+        printf("div by zero");                 //проверка дел.на 0
     }else{
-        printf("%lf\n", div(num1, num2));}
+        printf("%lf\n", div(num1, num2));       
+    }
 } else{
-    printf("вы что-то не то ввели");
+    printf("Error. Please, try again");           //проверка если ввели ерунду
 }
     return 0;
 }
