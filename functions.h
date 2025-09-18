774db05212e7e4291d60024622f306ef441bddc9
harun00000 <ut14kutsko41@gmail.com>
2025-09-18 18:54:11 +0300

    изм

---
+++ functions.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+double sum(double num1, double num2){
+    return num1+num2;
+}
+double diff(double num1, double num2){
+    return num1-num2;
+}
+double mult(double num1, double num2){
+    return num1*num2;
+}
+double div(double num1, double num2){
+    return num1/num2;
+}
--- main.c
+++ main.c
@@ -1,7 +1,24 @@
+#include <functions.c>
+double num1=0, num2=0;
+char operator=0; 
 
-#include <stdio.h>
-int main(void){
-    printf("Hello, world!");
+int main(){
+printf("Введите выражение\n");
+scanf("%lf %c %lf", &num1, &operator, &num2); //пользователь вводит числа
+
+if (operator == '+'){
+    printf("%lf\n", sum(num1, num2));
+} elif (operator == '-'){
+    printf("%lf\n", diff(num1, num2));
+} elif (operator== '*'){
+    printf("%lf\n", mult(num1, num2))
+} elif (operator=='/'){
+    if (num2==0){
+        printf("деление на 0");
+    }else{
+        printf("%lf\n", div(num1, num2));}
+} else{
+    printf("вы что-то не то ввели");
+}
     return 0;
-
 }
